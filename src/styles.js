e2c8e6f1ee136b5e32545960e4e842f7baf9e039
DESKTOP-4O4N2KM\Ann <antonellazacagnino@gmail.com>
2020-07-28 23:25:26 -0300

    Styles for login page

--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   body {
@@ -27,3 +28,38 @@ export const theme = {
   fontSecondary: '#828282',
   fontLight: '#CFCCCC'
 };
+
+export const LoginBox = styled.div`
+      padding: 10% 0 0 0;
+      text-align: center;
+      display: flex;
+      flex-direction: column; 
+`;
+
+export const LoginButton = styled.button`
+      place-self: center;
+      width: 386.34px;
+      height: 53px;
+      background: #FFFFFF;
+      border: solid 1px #FFF;
+      border-radius: 4px;
+      font-family: Roboto;
+      font-size: 14px;
+      color: #000000;
+      margin-top:20px;
+`;
+
+export const Logo = styled.img`
+      width: 388px;
+      align-self: center;
+`;
+
+export const BottomText = styled.p`
+      padding-top: 10%;
+      width: 394px;
+      font-family: Roboto;
+      font-size: 18px;
+      line-height: 21px;
+      place-self: center;
+      color: #CCE6ED;
+`;
