1da75f57e17b73d3f5fbb3706ebef8fda7648f2f
Shorsh <jorge@cliengo.com>
2020-08-02 15:15:51 -0300

    structuring

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ export default function App() {
     </RecoilRoot>
   ) : (
     <RecoilRoot>
-      <Routes />
+      <Router>
+        <Routes />
+      </Router>
     </RecoilRoot>
   );
 }
--- src/pages/Poll/Poll.js
+++ src/pages/Poll/Poll.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import VoteList from 'components/VoteList';
+import { isBrowser } from 'react-device-detect';
 import { usePoll } from './hooks';
 
 export default () => {
@@ -9,78 +10,99 @@ export default () => {
   ] = usePoll();
 
   if (loading) {
-    return <div className='pollContent'>
-      <div className='pollContainer'>
-        <div className="img"></div>
-        <div className="success">Loading...</div>
+    return (
+      <div className="pollContent">
+        <div className="pollContainer">
+          <div className="img" />
+          <div className="success">Loading...</div>
+        </div>
       </div>
-    </div>
+    );
   }
 
   if (voted) {
-    return <div className='pollContent'>
-      <div className='pollContainer'>
-        <div className="img"></div>
-        <div className="success">Ya votaste!</div>
+    return (
+      <div className="pollContent">
+        <div className="pollContainer">
+          <div className="img" />
+          <div className="success">Ya votaste!</div>
+        </div>
       </div>
-    </div>
-      
+    );
   }
 
   return (
-    <div className='pollContent'>
-      <div className='pollContainer'>
-        <div className="img"></div>
-        <div className="pollTitle">{pollName}</div>
-        <VoteList options={options} maxSelectable={maxSelectable} />
-        <div className="voteBtn">
-          <button type="button" onClick={submit} disabled={voted}>
-            Votar!
-          </button>
-        </div>
-      </div>
-      <div className="sideContent">
-        <div className="cardsContainer">
-          <h1>Cartas</h1>
-          <div className="content">
-            <p>Sistema de cartas aca tenes 3 oportunidades para agregar mas tiempo antes de la fecha limite de la finalizacion. </p>
-            <div className="cards">
-              <div className="card"></div>
-              <div className="card"></div>
-              <div className="card-rotated"></div>
-            </div>
+    <div className="pollContent">
+      {isBrowser ? (
+        <div className="pollContainer">
+          <div className="img" />
+          <div className="pollTitle">{pollName}</div>
+          <VoteList options={options} maxSelectable={maxSelectable} />
+          <div className="voteBtn">
+            <button type="button" onClick={submit} disabled={voted}>
+              Votar!
+            </button>
           </div>
         </div>
-        <div className="resultsContainer">
-          <h1>Resultados</h1>
-          <div className="resultsList">
-            <div className="result">
-              <div className="name">
-                asd
+      ) : (
+        <div className="pollContainer">
+          <div className="img" />
+          <div className="pollTitle">{pollName}</div>
+          <VoteList options={options} maxSelectable={maxSelectable} />
+          <div className="voteBtn">
+            <button type="button" onClick={submit} disabled={voted}>
+              Votar!
+            </button>
+          </div>
+        </div>
+      )}
+
+      {isBrowser && (
+        <div className="sideContent">
+          <div className="cardsContainer">
+            <h1>Cartas</h1>
+            <div className="content">
+              <p>
+                Sistema de cartas aca tenes 3 oportunidades para agregar mas
+                tiempo antes de la fecha limite de la finalizacion.
+              </p>
+              <div className="cards">
+                <div className="card" />
+                <div className="card" />
+                <div className="card-rotated" />
               </div>
-              <div className="progress"></div>
             </div>
-            <div className="result">
-              <div className="name">
-                sddd
+          </div>
+          <div className="resultsContainer">
+            <h1>Resultados</h1>
+            <div className="resultsList">
+              <div className="result">
+                <div className="name">asd</div>
+                <div className="progress" />
               </div>
-              <div className="progress"></div>
+              <div className="result">
+                <div className="name">sddd</div>
+                <div className="progress" />
+              </div>
+            </div>
+          </div>
+          <div className="infoContainer">
+            <h1>Info</h1>
+            <div>
+              <span className="infoDate">Fecha de Inicio: </span>
+              <span className="date">mm/dd/yy</span>
+            </div>
+            <div>
+              <span className="infoDate">Fecha de Finalizacion: </span>
+              <span className="date">mm/dd/yy</span>
+            </div>
+            <div>
+              <span className="infoDate">Fecha por Tiempo Extra: </span>
+              <span className="date">mm/dd/yy</span>
             </div>
           </div>
         </div>
-        <div className="infoContainer">
-          <h1>Info</h1>
-          <div>
-            <span className="infoDate">Fecha de Inicio: </span><span className="date">mm/dd/yy</span>
-          </div>
-          <div>
-            <span className="infoDate">Fecha de Finalizacion: </span><span className="date">mm/dd/yy</span>
-          </div>
-          <div>
-            <span className="infoDate">Fecha por Tiempo Extra: </span><span className="date">mm/dd/yy</span>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
